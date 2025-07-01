6be1441019ba1513fdafede32689fe0512a53495
Benjamin Kobjolke <b.kobjolke@xida.de>
2025-07-01 09:16:10 +0200

    FIX: compile

--- compile_exe.bat
+++ compile_exe.bat
@@ -1,5 +1,5 @@
 @echo off
 call activate_environment.bat
 call pyinstaller --name MinimalisticPomodoroTimer --onefile --windowed main.py
-xcopy settings.ini dist /E /Y
-xcopy serif_led_board-7.ttf dist /E /Y
+xcopy settings.ini dist /Y
+xcopy serif_led_board-7.ttf dist /Y
